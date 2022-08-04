4b8e26bca64e427d9abf363ec1ec31ea4e623605
matheuskassagas <gigliotti_@hotmail.com>
2022-08-04 15:30:51 -0300

    corrigindo erros

--- infra/autoscaling.tf
+++ infra/autoscaling.tf
@@ -7,17 +7,18 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
-  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
+  target_group_arns = var.producao ? [ aws_lb_target_group.alvoLoadBalancer[0].arn ] : []
 }
 
 resource "aws_autoscaling_policy" "this" {
  name = "terraform-escala"
  autoscaling_group_name = var.nomeGrupo
  policy_type = "TargetTrackingScaling"
- target_trackingtarget_tracking_configuration {
-   predefined_metric_type = "ASGAverageCPUUtilization"
-
+ target_tracking_configuration {
+  predefined_metric_specification {
+    predefined_metric_type = "ASGAverageCPUUtilization"
+  }
+   target_value = 50.0
  } 
- target_value = 50.0
-
+ count = var.producao ? 1 : 0
 }
--- infra/lb.tf
+++ infra/lb.tf
@@ -1,6 +1,7 @@
 resource "aws_lb" "loadBalancer" {
   internal = false # # falso pois comunica com a internet 
   subnets = [ aws_default_subnet.sub1.id, aws_default_subnet.sub2.id ] 
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_lb_target_group" "alvoLoadBalancer" {
@@ -8,14 +9,16 @@ resource "aws_lb_target_group" "alvoLoadBalancer" {
   port     = "8000"
   protocol = "HTTP" 
   vpc_id   = aws_default_vpc.default.id # # VPC DEFAULT AWS
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_lb_listener" "entradaLoadBalancer" {
-  load_balancer_arn = aws_lb.loadBalancer.arn
+  load_balancer_arn = aws_lb.loadBalancer[0].arn
   port = "8000"
   protocol = "HTTP"
   default_action {
     type = "forward"
-    target_group_arn = aws_autoscaling_group.alvoLoadBalancer.arn
+    target_group_arn = aws_autoscaling_group.alvoLoadBalancer[0].arn
   }
+  count = var.producao ? 1 : 0
 }
